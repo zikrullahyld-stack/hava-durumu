d06edeec8b2aef9c37b6ab458e4df47e52c9ae7f
zikrullahyld-stack <zikrullahyld@gmail.com>
2026-01-13 21:14:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+  card.addEventListener("touchstart", () => {
+    card.classList.add("pressed");
+    if(navigator.vibrate) navigator.vibrate(20);
+  });
+
+  card.addEventListener("touchend", () => {
+    card.classList.remove("pressed");
+  });
+
+  card.addEventListener("mousedown", () => {
+    card.classList.add("pressed");
+  });
+
+  card.addEventListener("mouseup", () => {
+    card.classList.remove("pressed");
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.classList.remove("pressed");
+  });
+
+});
+
 function openSite(url){
-  window.open(url, "_blank");
+  document.body.classList.add("fadeout");
+  setTimeout(() => {
+    window.open(url, "_blank");
+    document.body.classList.remove("fadeout");
+  }, 350);
 }
